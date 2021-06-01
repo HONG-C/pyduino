61c7363a914736986f93b93cc718fb23909db68a
HONG-C <jihun3900@naver.com>
2021-06-02 00:50:37 +0900

    py_talker 노드로 float64데이터형 퍼블리싱 성공,사용 아두이노 파일은 ros_lib의 test의 float64를 수정해서 사용

--- py_talker.py
+++ py_talker.py
@@ -3,15 +3,16 @@
 
 #python으로 아두이노를 제어하기 위한 퍼블리셔
 import rospy
-import random
 from std_msgs.msg import String
+from std_msgs.msg import Float64
 
+#데이터를 퍼블리싱하는 부분
 def talker():
-      pub = rospy.Publisher('toggle_led',String,queue_size=10)
+      pub = rospy.Publisher('your_topic',Float64,queue_size=10)
       rospy.init_node('py_talker', anonymous = True)
-      rate = rospy.Rate(10) #10hz
+      rate = rospy.Rate(60) #60hz,default is 10hz
       while not rospy.is_shutdown():
-	    test_val=1
+	    test_val=1.234
             rospy.loginfo(test_val)
             pub.publish(test_val)
             rate.sleep()
