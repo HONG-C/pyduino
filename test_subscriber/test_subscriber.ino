1e77c15be7eecc39eb9fd934dd2d59fccd5b96dc
HONG-C <jihun3900@naver.com>
2021-06-05 21:03:44 +0900

    test_subscriber 아두이노 파일을 수정하여 메시지가 정상적으로 전송되는것을 확인. 시리얼 창 상에서의 오류였던 듯.

--- test_subscriber/test_subscriber.ino
+++ test_subscriber/test_subscriber.ino
@@ -12,7 +12,7 @@ ros::NodeHandle nh;
 std_msgs::Float64 test;
 void messageCb( const std_msgs::Float64& msg){
   test.data = msg.data;
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
+  
 }
 
 ros::Subscriber<std_msgs::Float64> s("your_topic", &messageCb);
@@ -23,14 +23,20 @@ void setup()
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(s);
+  Serial.begin(57600);
 }
 
 void loop()
 {
-  if (sizeof(test.data)==4)
+  if(test.data==11.23)
   {
-    Serial.println(test.data);
+    Serial.println("ok");
+  }
+  else
+  {
+    Serial.println("not ok");
+    //Serial.println(test.data);
   }
   nh.spinOnce();
-  delay(60);
+  //delay(60);
 }
